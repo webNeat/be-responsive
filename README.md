85f1caae4114559fdef5f361e77e471ee51cbebc
Amine Ben hammou <webneat@gmail.com>
2015-05-27 16:39:27 +0200

    Version 1.0.0

---
+++ README.md
@@ -0,0 +1,22 @@
+#Responsive Table jQuery Plugin
+
+Responsive table by transforming each row into a table like style using CSS
+
+##Demo
+
+[Click here to see the demo](http://webneat.net/jquery/responsive-table/)
+
+##Install
+
+Clone this repository or download it as zip or install via bower
+
+	bower install responsive-table
+
+##Get Started
+
+Include the files `responsive-table.css` and `responsive-table.js`
+
+	<link rel="stylesheet" href="responsive-table.css">
+    <script src="responsive-table.js"></script>
+
+Then add the `be-responsive` class to any table. Be sure the table defines titles using `th` and not `td`. The `data-with-titles` could be added with value `"true"` to show titles on every row when the table is transformed ( [see the demo](http://webneat.net/jquery/responsive-table/) )
---
+++ responsive-table.js
@@ -0,0 +1,37 @@
+/**
+ * responsiveTable jQuery plugin
+ * @author Amine Ben hammou (webneat@gmail.com)
+ */
+(function($){
+	$.fn.responsiveTable = function(options) {
+		return this.each(function(){
+			var withTitles = $(this).attr('data-with-titles');
+			withTitles = ( withTitles === "true" ) ? true : false;
+			if(withTitles){
+				$(this).addClass('responsive-with-titles');
+				var id = $(this).attr('id');
+				if(id === undefined){
+					id = 'responsive-table-' + $.fn.responsiveTable.nextId;
+					$.fn.responsiveTable.nextId ++;
+					$(this).attr('id', id);
+				}
+
+				var css = '';
+				var i = 1;
+				$(this).find('th').each(function(){
+					css += '#' + id + ' td:nth-of-type(' + i + '):before { content: "' + $(this).html() + '"; } ';
+					i ++;
+				});
+				console.log(css);
+				$('head').append('<style> @media only screen and (max-width: 760px), (min-device-width: 760px) and (max-device-width: 1024px) { ' + css + ' } </style>');
+			}
+		});
+	};
+
+	$.fn.responsiveTable.nextId = 1;
+
+	$(document).ready(function(){
+		$('table.be-responsive').responsiveTable();
+	});
+
+}(jQuery));
